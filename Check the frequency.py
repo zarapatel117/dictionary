0f5ef4179a94ba4f200ff8275468fae3ed1621db
zarapatel117 <zarapatel2510@gmail.com~>
2025-01-21 20:39:55 +0530

    Check the frequency

---
+++ Check the frequency.py
@@ -0,0 +1,10 @@
+data={'coding':2,'for':1,'zara':1,'bye':2}
+print("orignal dictionary=",(str(data)))
+h=2
+
+res=0
+for key in data:
+    if data [key]==h:
+        res=res+1
+print("frecuency of h is",(str(res)))        
+
